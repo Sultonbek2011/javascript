ea2a34903997eab051a2a32cce356d51353515c2
Sultonbek2011 <ikmuborak@gmail.com>
2023-09-09 10:07:24 +0300

    first commit

---
+++ 2-dars/main.js
@@ -0,0 +1,73 @@
+function yoz7(){
+    var inp1 = document.getElementById('inp1');
+    console.log(inp1);
+    inp1.value += "7";
+ }
+ 
+ function yoz8(){
+    var inp1 = document.getElementById('inp1');
+    inp1.value += "8";
+ }
+ 
+ function bajar9(){
+    var inp1 = document.getElementById('inp1');
+    inp1.value += "9";
+ }
+ 
+ function yozPlus(){
+     var inp1 = document.getElementById('inp1');
+     inp1.value += "+";
+ }
+ 
+ function yoz4(){
+     var inp1 = document.getElementById('inp1');
+     inp1.value += "4";
+ }
+ 
+ function yoz5(){
+     var inp1 = document.getElementById('inp1');
+     inp1.value += "5";
+ }
+ function yoz6(){
+    var inp1 = document.getElementById('inp1');
+    inp1.value += "6";
+}
+function yozminus(){
+    var inp1 = document.getElementById('inp1');
+    inp1.value += "-";
+}
+function yoz1(){
+    var inp1 = document.getElementById('inp1');
+    inp1.value += "1";
+}
+function yoz2(){
+    var inp1 = document.getElementById('inp1');
+    inp1.value += "2";
+}
+function yoz3(){
+    var inp1 = document.getElementById('inp1');
+    inp1.value += "3";
+}
+function l(){
+    var inp1 = document.getElementById('inp1');
+    inp1.value += "*";
+}
+function yoz0(){
+    var inp1 = document.getElementById('inp1');
+    inp1.value += "0";
+}
+function k(){
+    var inp1 = document.getElementById('inp1');
+    inp1.value += ",";
+}
+function xisobla(){
+    var inp1 = document.getElementById('inp1');
+    console.log(inp1.value);
+    var result = eval(inp1.value);
+    console.log(result);
+    inp1.value = result;
+}
+function m(){
+    var inp1 = document.getElementById('inp1');
+    inp1.value += "/";
+}
